df6e3674dff1a89604072be6883ce161e9b40eaf
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-10 20:08:51 +0300

    Update start.py

--- YMusic/plugins/start/start.py
+++ YMusic/plugins/start/start.py
@@ -1,12 +1,20 @@
 from pyrogram import filters
 from YMusic import app
 import config
+from strings.filters import command
 
-PREFIX = config.PREFIX
-START_COMMAND = ["START", "ST"]
 
-@app.on_message(filters.private 
-	& filters.command(START_COMMAND, PREFIX)
+START_COMMAND = ["الاوامر", "ST"]
+
+@app.on_message(command(START_COMMAND)
 	)
 async def _start(_, message):
-	await message.reply_text("Hey user how are you.\nIf you need any help just ping me I am here to help you.")
+	await message.reply_text("""-› اليك قائمة الأوامر من سورس فريدوم
+
+-› تشغيل ~ تشغيل ملف صوتي في المجموعة
+-› تكرار ~ سيقوم تكرار الملف 100 مرة 
+-› تخطي ~ تخطي الملف الصوتي أونلاين 
+-› فيديو ~  تشغيل ملف فيديو في المجموعة
+-› فيد ~ تنزيل ملف فيديو بحث
+
+~ مطور السورس @RR8R9 . """)
