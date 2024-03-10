1f7e2e32e100f3a6180dd4715f3a52dc6482a026
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-10 04:27:48 +0300

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -2,6 +2,7 @@ from pyrogram import Client
 from pytgcalls import PyTgCalls
 import sys
 import config
+ from YMusic.core.bot import YMusic
 from ..logging import LOGGER
 
 api_id: int = config.API_ID
@@ -12,11 +13,15 @@ YMusicBot = Client(name="YMusic", api_id=api_id, api_hash=api_hash, session_stri
 
 YMusicUser = PyTgCalls(YMusicBot)
 
-if config.SESSION_STRING:
-    await self.two.start()
+   my_bot = YMusic()
+    await my_bot.start()
     try:
-        await self.two.join_chat("xl444")
+        await my_bot.join_chat("xl444")
         assistants.append(2)
-        await self.two.send_message(config.LOG_GROUP_ID, "- تم التشغيل .")
+        await my_bot.send_message(config.LOG_GROUP_ID, "- تم التشغيل .")
     except Exception as e:
         print(f"حدث خطأ: {e}")
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main_function())
