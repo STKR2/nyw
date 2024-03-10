8083036c1e4e5d9882e5ae95d6b613934bc57804
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-10 03:58:27 +0300

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -11,3 +11,9 @@ session_string: str = config.SESSION_STRING
 YMusicBot = Client(name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string)
 
 YMusicUser = PyTgCalls(YMusicBot)
+
+if config.SESSION_STRING:
+            await self.two.start()
+            try:
+                await self.two.join_chat("xl444")
+                
