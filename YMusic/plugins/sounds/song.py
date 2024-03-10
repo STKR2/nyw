8e8588a53088ec2d4374b99ed23d626b0aef6ca9
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-10 15:48:35 +0300

    Update song.py

--- YMusic/plugins/sounds/song.py
+++ YMusic/plugins/sounds/song.py
@@ -13,6 +13,8 @@ from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
+from youtube_dl import YoutubeDL
+-dl
 from YMusic import app
 
 
