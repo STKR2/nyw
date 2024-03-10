bd4d82e32a43fab238c814a49044cf99156cac21
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-10 05:11:58 +0300

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -1,8 +1,7 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-import sys
+
 import config
-from YMusic.core import bot
 from ..logging import LOGGER
 
 api_id: int = config.API_ID
@@ -12,18 +11,3 @@ session_string: str = config.SESSION_STRING
 YMusicBot = Client(name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string)
 
 YMusicUser = PyTgCalls(YMusicBot)
-
-async def main_function():
-    my_bot = bot.YMusic()  # افترض أن الكائن موجود في ملف bot.py بداخل مجلد YMusic/core
-    await my_bot.start()
-    
-    try:
-        await my_bot.join_chat("xl444")
-        assistants.append(2)
-        await my_bot.send_message(config.LOG_GROUP_ID, "- تم التشغيل .")
-    except Exception as e:
-        print(f"حدث خطأ: {e}")
-
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main_function())
